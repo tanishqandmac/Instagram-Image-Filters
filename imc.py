369ef6d27960f5d6775acee35b220e05d4685bb3
tanishqandmac <tanishq15105@iiitd.ac.in>
2018-10-18 13:28:44 +0530

    Added Part 1 and Part 2

---
+++ imc.py
@@ -0,0 +1,21 @@
+import cv2
+def increase_brightness(img, value=30):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+
+    lim = 255 - value
+    v[v > lim] = 255
+    v[v <= lim] += value
+
+    final_hsv = cv2.merge((h, s, v))
+    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    return img
+
+img_1 = cv2.imread('mammogram.jpg',3)
+img_2 = 255 - img_1
+cv2.imshow('input image',img_1)
+cv2.imshow('negative image',img_2)
+img_3 = increase_brightness(img_1,30)
+cv2.imshow('brightness increased',img_3)
+cv2.waitKey(100000)
+cv2.destroyAllWindows()
